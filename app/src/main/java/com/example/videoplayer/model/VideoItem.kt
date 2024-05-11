463dd5b133fbdf75984931443ceddec0106b19a9
Gajanand <gajanand@Gajanands-MacBook-Pro.local>
2024-05-11 10:27:47 +0530

    video player started

---
+++ app/src/main/java/com/example/videoplayer/model/VideoItem.kt
@@ -0,0 +1,6 @@
+package com.example.videoplayer.model
+
+import android.net.Uri
+import androidx.media3.common.MediaItem
+
+data class VideoItem(val contentUri: Uri, val mediaItem: MediaItem, val name: String)
